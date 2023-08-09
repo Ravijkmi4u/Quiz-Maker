8ee1ae5f792b7a70c7f5f4c3f1603b9a0756ce39
ravirkumar2 <ravi.kumar5@soprasteria.com>
2023-08-09 17:55:54 +0530

    remove unused code

--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -1,21 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, ReplaySubject, Subject, filter, map } from 'rxjs';
-import { Questions, QuizList, TriviaCategories } from '../models/quiz.model';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+import { PayloadRequest, Questions, QuizList, TriviaCategories } from '../models/quiz.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
   submitQuiz$ = new BehaviorSubject<Questions[]>([]);
-  showHideCatergoryFilter$ = new BehaviorSubject<boolean>(true);
   constructor(private readonly http: HttpClient) { }
 
   getQuizCategoriesList(): Observable<TriviaCategories> {
     return this.http.get<TriviaCategories>('https://opentdb.com/api_category.php');
   }
 
-  getQuizList(payload: any): Observable<Questions[]> {
+  getQuizList(payload: PayloadRequest): Observable<Questions[]> {
     return this.http.get<QuizList>(`https://opentdb.com/api.php?amount=${payload.amount}&category=${payload.category}&difficulty=${payload.difficulty}&type=${payload.type}`).pipe(
       map(data => data.results));
   }
